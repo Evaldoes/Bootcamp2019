a0082886165516afd8af783385204885543e5400
Evaldo Cardoso <Evaldopkscardoso@gmail.com>
2020-01-19 22:57:55 -0400

    boot

--- modulo01/index.js
+++ modulo01/index.js
@@ -13,6 +13,34 @@ const users = ['evaldo', 'fabio', 'isaque', 'STRNeoh'];
 //requisição para o servidor
 //req => é todos os dados da requisição
 //rep => é todas as informações, que são necessarias p/ retornar uma resposta para o cliente
+//middleware são utilizados  pra controle de fluxo
+
+//middleware Global
+server.use((req,res,next) => {
+    console.time('Request');
+    console.log(`Método: ${req.method}; URL: ${req.url}`);
+    next();
+    console.timeEnd('Request');
+})
+
+
+//midle LOcal
+function checkUserExists(req,res,next){
+    if (!req.body.user){
+        return res.status(400).json({error: 'User name is required'});
+    }
+
+    return next();
+}
+
+
+function checkUserInArray(req,res,next){
+    if (!users[req.params.index]){
+        return res.status(400).json({error: 'User does not exists'});
+    }
+
+    return next();
+}
 
 
 //retorna apenasum usuarios especifico
@@ -21,21 +49,21 @@ server.get('/users', (req,res) => {
 })
 
 //retorna todos os usuarios
-server.get('/users/:index', (req,res) => {
+server.get('/users/:index', checkUserInArray, (req,res) => {
     // return res.send('Hello World');
     const { index } = req.params;
     return res.json(users[index]);
 })
 
 //criar um novo usuario
-server.post("/users", (req, res) => {
+server.post("/users", checkUserExists, (req, res) => {
     const {name} = req.body;
     users.push(name);
     return res.json(users);
 })
 
 //edita usuario
-server.put("/users/:index",(req,res) => {
+server.put("/users/:index", checkUserExists, checkUserInArray, (req,res) => {
     const { index } = req.params;
     const { name } = req.body;
     users[index] = name;
@@ -43,7 +71,7 @@ server.put("/users/:index",(req,res) => {
 })
 
 //deleta usuario
-server.delete("/users/:index",(req,res) => {
+server.delete("/users/:index", checkUserInArray, (req,res) => {
     const { index } = req.params;
     users.splice(index, 1);
     return res.json(users);
